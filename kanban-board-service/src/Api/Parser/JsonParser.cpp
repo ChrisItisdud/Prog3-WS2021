ece68d1da58be1fad1905ec052e54ad0f6c106c1
ChrisItisdud <chris.reichel@gmx.net>
2021-10-28 14:30:49 +0200

    implemented convertToApiString and convertItem/ColumnToModel for item and model

--- kanban-board-service/src/Api/Parser/JsonParser.cpp
+++ kanban-board-service/src/Api/Parser/JsonParser.cpp
@@ -16,7 +16,21 @@ string JsonParser::convertToApiString(Board &board) {
 }
 
 string JsonParser::convertToApiString(Column &column) {
-    throw NotImplementedException();
+    std::string items = "\"items\": [";
+    for (Item i : column.getItems()) {
+        items.append("\n");
+        items.append(convertToApiString(i));
+    }
+    items.append("\n]");
+    return "{\n\"id\": " +
+           std::to_string(column.getId()) +
+           ",\n\"name\": \"" +
+           column.getName() +
+           "\",\n\"position\": " +
+           std::to_string(column.getPos()) +
+           ",\n" +
+           items +
+           "\n}";
 }
 
 string JsonParser::convertToApiString(std::vector<Column> &columns) {
@@ -24,7 +38,15 @@ string JsonParser::convertToApiString(std::vector<Column> &columns) {
 }
 
 string JsonParser::convertToApiString(Item &item) {
-    throw NotImplementedException();
+    return "{\n\"id\": " +
+           std::to_string(item.getId()) +
+           ",\n\"title\": \"" +
+           item.getTitle() +
+           "\",\n\"position\": " +
+           std::to_string(item.getPos()) +
+           ",\n\"timestamp\": \"" +
+           item.getTimestamp() +
+           "\"\n}";
 }
 
 string JsonParser::convertToApiString(std::vector<Item> &items) {
@@ -32,9 +54,35 @@ string JsonParser::convertToApiString(std::vector<Item> &items) {
 }
 
 std::optional<Column> JsonParser::convertColumnToModel(int columnId, std::string &request) {
-    throw NotImplementedException();
+    //Get name
+    std::size_t nam1 = request.find_first_of("name");
+    std::string nam = request.substr(nam1);
+    nam1 = nam.find_first_of(",");
+    nam = nam.substr(8, static_cast<int>(nam1) - 2);
+    //Get position
+    std::size_t pos1 = request.find_first_of("positio");
+    std::string pos = request.substr(pos1);
+    pos1 = pos.find_first_of(",");
+    pos = pos.substr(11, static_cast<int>(pos1) - 1);
+    int position = std::stoi(pos);
+    //return result
+    std::optional<Column> result = Column(columnId, nam, position);
+    return result;
 }
 
 std::optional<Item> JsonParser::convertItemToModel(int itemId, std::string &request) {
-    throw NotImplementedException();
+    //Get title
+    std::size_t title1 = request.find_first_of("title");
+    std::string title = request.substr(title1);
+    title1 = title.find_first_of(",");
+    title = title.substr(9, static_cast<int>(title1) - 2);
+    //Get position
+    std::size_t pos1 = request.find_first_of("positio");
+    std::string pos = request.substr(pos1);
+    pos1 = pos.find_first_of(",");
+    pos = pos.substr(11, static_cast<int>(pos1) - 1);
+    int position = std::stoi(pos);
+    //return result
+    std::optional<Item> result = Item(itemId, title, position, "");
+    return result;
 }
